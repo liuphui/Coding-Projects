52d3df6ced587d4281df37308306f29c182c1ed7
Peng Hui Liu <liup1@tcd.ie>
2024-08-04 16:31:28 +0100

    deleted unnecessary code

--- Snake_Game/GameFrame.java
+++ Snake_Game/GameFrame.java
@@ -5,14 +5,6 @@ import javax.swing.JFrame;
  */
 public class GameFrame extends JFrame {
     GameFrame(){
-        // this.add(new StartingMenu());
-        // this.setTitle("Starting Menu");
-        // this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        // this.setResizable(false);
-        // this.pack();
-        // this.setVisible(true);
-        // this.setLocationRelativeTo(null);
-
         this.add(new GamePanel());
         this.setTitle("Snake Game");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
